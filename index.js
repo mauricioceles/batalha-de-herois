392fc515280e58c3355345e2210b2bbd349fb336
mauricioceles <mauricioceles@gmail.com>
2023-10-05 17:27:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome
+        this.idade
+        this.tipo
+
+    }
+
+    atacar(tipo) {
+        let ataque
+
+        if (tipo == "mago") {
+            ataque = "magia"
+        }
+        else if (tipo == "guerreiro") {
+            ataque = "espada"
+        }
+        else if (tipo == "monge") {
+            ataque = "artes marciais"
+        }
+        else {
+            ataque = "shuriken"
+        }
+
+        console.log("O " + tipo + " atacou usando " + ataque)
+    }
+}
+
+let herois = ["mago", "guerreiro", "monge", "ninja"]
+let heroi = new Heroi()
+
+for (let i in herois) {
+    heroi.atacar(herois[i])
+}
